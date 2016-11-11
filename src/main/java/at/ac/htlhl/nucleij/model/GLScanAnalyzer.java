89374f420adca1e04df4a1cc5734f865ea2ac9c8
Stefan Erben <stefan.erben1@gmail.com>
2016-11-11 09:48:18 +0100

    Added Src

---
+++ src/main/java/at/ac/htlhl/nucleij/model/GLScanAnalyzer.java
@@ -0,0 +1,9 @@
+package at.ac.htlhl.nucleij.model;
+
+/**
+ * Created by Stefan on 11.11.2016.
+ */
+public class GLScanAnalyzer
+{
+    //test kommentar
+}
---
+++ src/main/java/at/ac/htlhl/nucleij/model/NdpiConverter.java
@@ -0,0 +1,9 @@
+package at.ac.htlhl.nucleij.model;
+
+/**
+ * Created by Stefan on 11.11.2016.
+ */
+public class NdpiConverter
+{
+    //erster Kommentar
+}
