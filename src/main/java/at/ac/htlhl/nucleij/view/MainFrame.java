d9cc8d678fe93004bdbb1a6aa54e581eb7238c5a
Andreas Mattes <andi.mattes97@gmail.com>
2016-11-11 13:28:44 +0100

    Edited MainFrame

--- src/main/java/at/ac/htlhl/nucleij/view/MainFrame.java
+++ src/main/java/at/ac/htlhl/nucleij/view/MainFrame.java
@@ -27,21 +27,87 @@ public class MainFrame extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        label1 = new JLabel();
+        menuBar1 = new JMenuBar();
+        menu1 = new JMenu();
+        menu2 = new JMenu();
+        toolBar1 = new JToolBar();
+        button1 = new JButton();
+        button2 = new JButton();
+        button3 = new JButton();
+        tabbedPane1 = new JTabbedPane();
+        panel1 = new JPanel();
+        panel2 = new JPanel();
 
         //======== this ========
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
 
-        //---- label1 ----
-        label1.setText("NucleiJ");
-        contentPane.add(label1, BorderLayout.CENTER);
+        //======== menuBar1 ========
+        {
+
+            //======== menu1 ========
+            {
+                menu1.setText("File");
+            }
+            menuBar1.add(menu1);
+
+            //======== menu2 ========
+            {
+                menu2.setText("Help");
+            }
+            menuBar1.add(menu2);
+        }
+        setJMenuBar(menuBar1);
+
+        //======== toolBar1 ========
+        {
+            toolBar1.setFloatable(false);
+
+            //---- button1 ----
+            button1.setText("Convert & Analyze");
+            toolBar1.add(button1);
+
+            //---- button2 ----
+            button2.setText("Analyse");
+            toolBar1.add(button2);
+
+            //---- button3 ----
+            button3.setText("Convert");
+            toolBar1.add(button3);
+        }
+        contentPane.add(toolBar1, BorderLayout.NORTH);
+
+        //======== tabbedPane1 ========
+        {
+
+            //======== panel1 ========
+            {
+                panel1.setLayout(new BorderLayout());
+            }
+            tabbedPane1.addTab("Converter", panel1);
+
+            //======== panel2 ========
+            {
+                panel2.setLayout(new BorderLayout());
+            }
+            tabbedPane1.addTab("Analyzer", panel2);
+        }
+        contentPane.add(tabbedPane1, BorderLayout.CENTER);
         pack();
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    private JLabel label1;
+    private JMenuBar menuBar1;
+    private JMenu menu1;
+    private JMenu menu2;
+    private JToolBar toolBar1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JPanel panel2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
