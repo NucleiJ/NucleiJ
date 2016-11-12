4d3f68521eeffbcc51126ffd9c4e3cebe6dffc71
Stefan Erben <stefan.erben1@gmail.com>
2016-11-12 20:51:50 +0100

    Add Domain Object

--- src/main/java/at/ac/htlhl/nucleij/model/GLScanAnalyzer.java
+++ src/main/java/at/ac/htlhl/nucleij/model/GLScanAnalyzer.java
@@ -3,13 +3,100 @@ package at.ac.htlhl.nucleij.model;
 import com.jgoodies.binding.beans.Model;
 
 /**
- * Created by Stefan on 11.11.2016.
+ *
+ * @author Stefan Erben
+ * @version 1.0
  */
 public class GLScanAnalyzer extends Model
 {
-    //test kommentar
+    // region Constants
+    // ************************************************************************
+    // Properties
+    public static final String PROPERTY_INPUTPATH = "inputpath";
+    public static final String PROPERTY_OUTPUTPATH = "outputpath";
+    public static final String PROPERTY_MAGNIFICATION = "magnification";
+    public static final String PROPERTY_GENERATEHEATMAP = "generateheatmap";
+
+    // XXX Ist magnification nicht eigentlich Object? Jetzt mit String geloest
+    // endregion
+
+
+    /*
+    // TODO Erweiterung von JGoodies Beispiel fuer ObjectChoice
+    // An int based enumeration, Zeile 38..
+    public static final Integer X10   = 10;
+    public static final Integer X40 = 40;
+    static final Integer[] INTEGER_CHOICES =
+            { X10, X40 };
+    */
+
+    // region Fields
+    // ************************************************************************
+    private String inputpath;
+    private String outputpath;
+    private String magnification;
+    private boolean generateheatmap;
+    // endregion
+
+
     public GLScanAnalyzer()
     {
         super();
+
+        this.inputpath = "";
+        this.outputpath = "";
+        this.magnification = "x10";
+        this.generateheatmap = false;
     }
+
+    // region Getter and Setter
+    // ************************************************************************
+    public String getInputpath()
+    {
+        return inputpath;
+    }
+
+    public void setInputpath(String inputpath)
+    {
+        String oldValue = this.inputpath;
+        this.inputpath = inputpath;
+        firePropertyChange(PROPERTY_INPUTPATH, oldValue, inputpath);
+    }
+
+    public String getOutputpath()
+    {
+        return outputpath;
+    }
+
+    public void setOutputpath(String outputpath)
+    {
+        String oldValue = this.outputpath;
+        this.outputpath = outputpath;
+        firePropertyChange(PROPERTY_OUTPUTPATH, oldValue, outputpath);
+    }
+
+    public Object getMagnification()
+    {
+        return magnification;
+    }
+
+    public void setMagnification(String magnification)
+    {
+        String oldValue = this.magnification;
+        this.magnification = magnification;
+        firePropertyChange(PROPERTY_MAGNIFICATION, oldValue, magnification);
+    }
+
+    public boolean isGenerateheatmap()
+    {
+        return generateheatmap;
+    }
+
+    public void setGenerateheatmap(boolean generateheatmap) {
+        boolean oldValue = this.generateheatmap;
+        this.generateheatmap = generateheatmap;
+        firePropertyChange(PROPERTY_GENERATEHEATMAP, oldValue, generateheatmap);
+    }
+
+    // endregion
 }
--- src/main/java/at/ac/htlhl/nucleij/presenter/GLScanAnalyzerPM.java
+++ src/main/java/at/ac/htlhl/nucleij/presenter/GLScanAnalyzerPM.java
@@ -4,7 +4,9 @@ import at.ac.htlhl.nucleij.model.GLScanAnalyzer;
 import com.jgoodies.binding.PresentationModel;
 
 /**
- * Created by Stefan on 11.11.2016.
+ *
+ * @author Stefan Erben
+ * @version 1.0
  */
 public class GLScanAnalyzerPM extends PresentationModel<GLScanAnalyzer>
 {
