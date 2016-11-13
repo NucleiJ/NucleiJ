5ef5bfc717c39425da1b0fbe250dec616e113f1e
Stefan Erben <stefan.erben1@gmail.com>
2016-11-13 11:34:01 +0100

    Add Git Ignore File, started designing with GUI GLScanAnalyzerView, tried to outsource Texts, but could not create a .propotries File in the right directory

--- src/main/java/at/ac/htlhl/nucleij/view/GLScanAnalyzerView.java
+++ src/main/java/at/ac/htlhl/nucleij/view/GLScanAnalyzerView.java
@@ -4,11 +4,14 @@
 
 package at.ac.htlhl.nucleij.view;
 
-import javax.swing.*;
-
+import at.ac.htlhl.nucleij.model.GLScanAnalyzer;
 import at.ac.htlhl.nucleij.presenter.GLScanAnalyzerPM;
+import com.jgoodies.binding.binder.Binders;
+import com.jgoodies.binding.binder.PresentationModelBinder;
 import com.jgoodies.forms.factories.*;
 import com.jgoodies.forms.layout.*;
+
+import javax.swing.*;
 
 /**
  * @author Schülerlizenz 2016/17
@@ -26,19 +29,43 @@ public class GLScanAnalyzerView extends JPanel
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         label1 = new JLabel();
+        textField1 = new JTextField();
+        button1 = new JButton();
+        label2 = new JLabel();
+        textField2 = new JTextField();
+        button2 = new JButton();
 
         //======== this ========
         setLayout(new FormLayout(
-            "default, $lcgap, 101dlu",
-            "2*(default, $lgap), default"));
+            "23dlu, $lcgap, 47dlu, $lcgap, 63dlu, $lcgap, default:grow",
+            "18dlu, 5*($lgap, default)"));
 
         //---- label1 ----
-        label1.setText("Zeig mir den TEST");
-        add(label1, CC.xy(3, 5));
+        label1.setText("text");
+        add(label1, CC.xy(3, 3));
+        add(textField1, CC.xy(5, 3));
+
+        //---- button1 ----
+        button1.setText("text");
+        add(button1, CC.xy(7, 3));
+
+        //---- label2 ----
+        label2.setText("text");
+        add(label2, CC.xy(3, 5));
+        add(textField2, CC.xy(5, 5));
+
+        //---- button2 ----
+        button2.setText("text");
+        add(button2, CC.xy(7, 5));
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     private JLabel label1;
+    private JTextField textField1;
+    private JButton button1;
+    private JLabel label2;
+    private JTextField textField2;
+    private JButton button2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
