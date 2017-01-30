7f5d1918936b730dc1d9c5057aca2f7e8802691c
Stefan Erben <stefan.erben1@gmail.com>
2017-01-30 20:21:45 +0100

    ConverterProblem: GELÖST funktioniert auch schon bei der erstellten NucleiJ.jar Datei

--- src/main/java/at/ac/htlhl/nucleij/presenter/tasks/AnalyzerConverterTask.java
+++ src/main/java/at/ac/htlhl/nucleij/presenter/tasks/AnalyzerConverterTask.java
@@ -11,11 +11,12 @@ import org.jdesktop.application.SingleFrameApplication;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.*;
-import java.net.URLDecoder;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,7 +24,8 @@ import java.util.logging.Logger;
  * Created by andreas on 23.01.17.
  */
 
-public class AnalyzerConverterTask extends SwingWorker<String, String> {
+public class AnalyzerConverterTask extends SwingWorker<String, String>
+{
     private static final Logger LOGGER = Logger.getLogger(AnalyzerConverterTask.class.getName());
 
     private JProgressBar progressBar;
@@ -31,7 +33,8 @@ public class AnalyzerConverterTask extends SwingWorker<String, String> {
     private GLScanAnalyzer glScanAnalyzer;
     private NdpiConverter ndpiConverter;
 
-    public AnalyzerConverterTask(JProgressBar progressBar, TaskDialog taskDialog, NdpiConverter ndpiConverter, GLScanAnalyzer glScanAnalyzer) {
+    public AnalyzerConverterTask(JProgressBar progressBar, TaskDialog taskDialog, NdpiConverter ndpiConverter, GLScanAnalyzer glScanAnalyzer)
+    {
         this.progressBar = progressBar;
         this.taskDialog = taskDialog;
         this.ndpiConverter = ndpiConverter;
@@ -52,28 +55,26 @@ public class AnalyzerConverterTask extends SwingWorker<String, String> {
 
         switch (choice) {
             case 0:
-                add = 100 / (numberNdpiFiles * 2 + numberTifFiles);
+                add = 100/(numberNdpiFiles*2+numberTifFiles);
                 taskDialog.setInstruction("Converting & analyzing your Scans...");
                 break;
-            case 1:
-                add = 100 / numberNdpiFiles;
+            case 1: add = 100/numberNdpiFiles;
                 taskDialog.setInstruction("Converting your Scans...");
                 break;
-            case 2:
-                add = 100 / numberTifFiles;
+            case 2: add = 100/numberTifFiles;
                 taskDialog.setInstruction("Analyzing your Scans...");
                 break;
-            default:
-                add = 0;
+            default: add = 0;
         }
 
         if (choice != 2) { // Sicher Konvertieren
             // Konvertieren & an TifListe anhängen
-            for (String ndpiListElement : ndpiFileList) {
+            for (String ndpiListElement : ndpiFileList)
+            {
                 numberNdpiFiles--;
 
                 //element = ndpiListElement.substring(ndpiListElement.lastIndexOf(File.separator)+1, ndpiListElement.lastIndexOf("]"));// + "\n" + "Remaining: " + numberNdpiFiles + "NDPIs & " + numberTifFiles + "TIFs";
-                publish(ndpiListElement.substring(ndpiListElement.lastIndexOf(File.separator) + 1));
+                publish(ndpiListElement.substring(ndpiListElement.lastIndexOf(File.separator)+1));
                 //taskDialog.setText("File: " + String.valueOf(ndpiListElement).substring(String.valueOf(ndpiListElement).lastIndexOf(File.separator)+1, String.valueOf(ndpiListElement).lastIndexOf("]")));
                 //+ "\n" + "Remaining: " + numberNdpiFiles + "NDPIs & " + numberTifFiles + "TIFs");
 
@@ -87,16 +88,17 @@ public class AnalyzerConverterTask extends SwingWorker<String, String> {
 
         if (choice != 1) { // Sicher Analysieren
             tifFileList = glScanAnalyzer.getTifList();      //Aktualisierte Liste holen!!!
-            for (String tifListElement : tifFileList) {
+            for (String tifListElement : tifFileList)
+            {
                 numberTifFiles--;
 
                 //element = tifListElement.substring(tifListElement.lastIndexOf(File.separator)+1, tifListElement.lastIndexOf("]"));// + "\n" + "Remaining: " + numberNdpiFiles + "NDPIs & " + numberTifFiles + "TIFs";
-                publish(tifListElement.substring(tifListElement.lastIndexOf(File.separator) + 1));
+                publish(tifListElement.substring(tifListElement.lastIndexOf(File.separator)+1));
                 //taskDialog.setText("File: " + String.valueOf(tifListElement).substring(String.valueOf(tifListElement).lastIndexOf(File.separator)+1, String.valueOf(tifListElement).lastIndexOf("]")));
                 // + "\n" + "Remaining: " + numberNdpiFiles + "NDPIs & " + numberTifFiles + "TIFs");
 
                 mainAnalyzer.setDateiname(tifListElement);
-                System.out.println("\n****************\n" + tifListElement + "\n********************");
+                System.out.println("\n****************\n"+tifListElement+"\n********************");
                 mainAnalyzer.run(null);
                 currentStatus = currentStatus + add;
 
@@ -111,7 +113,7 @@ public class AnalyzerConverterTask extends SwingWorker<String, String> {
     }
 
     @Override
-    protected void process(List<String> element) {
+    protected void process(List <String> element) {
         super.process(element);
 
         taskDialog.setText(element.toString());
@@ -126,89 +128,115 @@ public class AnalyzerConverterTask extends SwingWorker<String, String> {
     }
 
     private void startConverter(String filePath) {
+        String OS = System.getProperty("os.name").toLowerCase();
+        File outputpath = new File(ndpiConverter.getOutputpath());
 
-        String testwhereJARisLocated = NucleiJ.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        String decodedTestwhereJARisLocated = testwhereJARisLocated;
-        try {
-            decodedTestwhereJARisLocated = URLDecoder.decode(testwhereJARisLocated, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+        File jarPath = new File("lib/ndpi-converter/ndpi-converter.jar");
+        String absolutePathofNdpiJar = jarPath.getAbsolutePath();
+        if (jarPath.exists())
+        {
+            absolutePathofNdpiJar = jarPath.getAbsolutePath();
         }
-        System.out.println("JAR wird unter dem Pfad: " + decodedTestwhereJARisLocated.concat("ndpi-converter.jar") + " gesucht!!");
-
-        File converterFile = new File(decodedTestwhereJARisLocated.concat("ndpi-converter.jar"));
-        String absolutePathofNdpiJar = converterFile.getAbsolutePath();
-
-        if (converterFile.exists()) {
-            //Pfad ausaehlen
-            System.out.println("Konverter JAR selber auswaehlen");
-
-            JFrame parent = ((SingleFrameApplication) Application.getInstance()).getMainFrame();
-            JFileChooser chooser = new JFileChooser();
-
-            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            chooser.setControlButtonsAreShown(true);
-            chooser.setDialogTitle("Konverter JAR Auswählen");
-            chooser.addChoosableFileFilter(new FileNameExtensionFilter("jar"));
-
-            if (chooser.showDialog(parent, "Auswählen") == JFileChooser.APPROVE_OPTION) {
-                if (chooser.getSelectedFile().isFile()) {
-                    if (ndpiConverter.getOutputpath().isEmpty()) {
-                        ndpiConverter.setOutputpath(ndpiConverter.getInputpath().substring(0, ndpiConverter.getInputpath().lastIndexOf(File.separator)).concat(File.separator + "Output"));
-                    }
-                    absolutePathofNdpiJar = chooser.getSelectedFile().getAbsolutePath();
-                }
-            }
-            else
-            {
-                System.out.println("keine JAR ausgewaehlt!");
-            }
-        }
-
-            File outputpath = new File(ndpiConverter.getOutputpath());
-
-            System.out.println(absolutePathofNdpiJar);
-            System.out.println(filePath);
-            System.out.println(outputpath.getParent());
-
-            //absolutePathofNdpiJar = "/home/stefan/Desktop/ndpi-to-ome-tiff-converter-v1.5/ndpi-converter.jar";
-            Process p;
-
-            //absolutePathofNdpiJar = "/home/andreas/IdeaProjects/nucleij/lib/ndpi-converter/ndpi-converter.jar";
-
-            String OS = System.getProperty("os.name").toLowerCase();
+        else
+        {
             try {
-                if (OS.contains("linux")) {
-                    System.out.println("Your OS is Linux");
-                    p = Runtime.getRuntime().exec("sudo java -jar " + absolutePathofNdpiJar + "\" -i 2 -c lzw -s \"" + filePath + "\" \"" + outputpath.getParent() + "\"");
-                    //p = Runtime.getRuntime().exec("sudo java -jar \"" + absolutePathofNdpiJar + "\" -i 2 -c lzw -s \""+ filePath + "\" \"" + outputpath.getParent() + "\"");
-                    //p = Runtime.getRuntime().exec("sudo java -jar /home/andreas/IdeaProjects/nucleij/lib/ndpi-converter/ndpi-converter.jar -i 2 -c lzw -s /home/andreas/Schreibtisch/Scans/N2700-14\\ 5\\ HE\\ -\\ 2016-06-06\\ 14.57.00.ndpi");
-                    //p = Runtime.getRuntime().exec("sudo java -jar /home/andreas/IdeaProjects/nucleij/lib/ndpi-converter/ndpi-converter.jar -i 2 -c lzw -s /home/andreas/Schreibtisch/Scans/N2700-14 5 HE - 2016-06-06 14.57.00.ndpi");
+                File newjarPath = new File(NucleiJ.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
 
-                    // Befehl auf Linux:
-                    // FUNKTIONIERT Erben: java -jar "/home/stefan/Desktop/ndpi-to-ome-tiff-converter-v1.5/ndpi-converter.jar"  -i 2 -c lzw -s  "/home/stefan/Desktop/test Scan.ndpi"
-                    // sudo java -jar /home/andreas/IdeaProjects/nucleij/lib/ndpi-converter/ndpi-converter.jar -i 2 -c lzw -s /home/andreas/Schreibtisch/Scans/N2700-14\ 5\ HE\ -\ 2016-06-06\ 14.57.00.ndpi
-                } else if (OS.contains("windows")) {
-                    System.out.println("Your OS is Windows");
-                    p = Runtime.getRuntime().exec("java -jar \"" + absolutePathofNdpiJar + "\" -i 2 -c lzw -s \"" + filePath + "\" \"" + outputpath.getParent() + "\"");
-                } else if (OS.contains("mac")) {
-                    p = Runtime.getRuntime().exec("java -jar \"" + absolutePathofNdpiJar + "\" -i 2 -c lzw -s \"" + filePath + "\" \"" + outputpath.getParent() + "\"");
-                    System.out.println("Your OS is Mac OS");
-                } else {
-                    System.out.println("Your OS is not supported!");
-                    //TODO Möglicherweise ConvertProzess beenden und not supported Dialog anzeigen
-                    p = null;
+                //concat(File.separator).concat("ndpi-converter").concat(File.separator).concat("ndpi-converter.jar"));
+                System.out.println("Pfad der JAR: " + newjarPath.getParent().concat(File.separator).concat("ndpi-converter").concat(File.separator).concat("ndpi-converter.jar"));
+                if (newjarPath.exists())
+                {
+                    absolutePathofNdpiJar = newjarPath.getParent().concat(File.separator).concat("ndpi-converter").concat(File.separator).concat("ndpi-converter.jar");
+                    System.out.println("Pfad der JAR: " + absolutePathofNdpiJar);
                 }
+                else
+                {
+                    newjarPath = null;
+                    if (OS.contains("linux")) {
+                        System.out.println("Your OS is Linux");
+                    }
+                    else if (OS.contains("windows")) {
+                        System.out.println("Your OS is Windows");
+                        newjarPath = new File("C:\\Program Files\\NucleiJ\\ndpi-converter.jar");
+                    }
+                    else if (OS.contains("mac")) {
+                        System.out.println("Your OS is Mac OS");
+                    }
+                    else {
+                        System.out.println("Your OS is not supported!");
+                    }
 
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(p.getInputStream()));
-                String line = null;
-                while ((line = in.readLine()) != null) {
-                    System.out.println(line);
+                    if(newjarPath.exists())
+                    {
+                        absolutePathofNdpiJar = newjarPath.getAbsolutePath();
+                    }
+                    else {
+                        JFrame parent = ((SingleFrameApplication) Application.getInstance()).getMainFrame();
+
+                        JFileChooser fileChooser = new JFileChooser();
+                        fileChooser.setCurrentDirectory(new File(System.getProperty(newjarPath.getAbsolutePath())));
+                        fileChooser.setFileFilter(new FileNameExtensionFilter("*.jar", "jar"));
+
+
+                        int result = fileChooser.showOpenDialog(parent);
+                        if (result == JFileChooser.APPROVE_OPTION) {
+                            File selectedFile = fileChooser.getSelectedFile();
+                            absolutePathofNdpiJar = selectedFile.getAbsolutePath();
+                        }
+                    }
+
                 }
-            } catch (IOException e) {
+            } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
+
+
+        }
+
+        System.out.println(absolutePathofNdpiJar);
+        System.out.println(filePath);
+        System.out.println(outputpath.getParent());
+
+        //absolutePathofNdpiJar = "/home/stefan/Desktop/ndpi-to-ome-tiff-converter-v1.5/ndpi-converter.jar";
+        Process p;
+
+        //absolutePathofNdpiJar = "/home/andreas/IdeaProjects/nucleij/lib/ndpi-converter/ndpi-converter.jar";
+
+        try {
+            if (OS.contains("linux")) {
+                System.out.println("Your OS is Linux");
+                p = Runtime.getRuntime().exec("sudo java -jar " + absolutePathofNdpiJar + "\" -i 2 -c lzw -s \"" + filePath + "\" \"" + outputpath.getParent() + "\"");
+                //p = Runtime.getRuntime().exec("sudo java -jar \"" + absolutePathofNdpiJar + "\" -i 2 -c lzw -s \""+ filePath + "\" \"" + outputpath.getParent() + "\"");
+                //p = Runtime.getRuntime().exec("sudo java -jar /home/andreas/IdeaProjects/nucleij/lib/ndpi-converter/ndpi-converter.jar -i 2 -c lzw -s /home/andreas/Schreibtisch/Scans/N2700-14\\ 5\\ HE\\ -\\ 2016-06-06\\ 14.57.00.ndpi");
+                //p = Runtime.getRuntime().exec("sudo java -jar /home/andreas/IdeaProjects/nucleij/lib/ndpi-converter/ndpi-converter.jar -i 2 -c lzw -s /home/andreas/Schreibtisch/Scans/N2700-14 5 HE - 2016-06-06 14.57.00.ndpi");
+
+                // Befehl auf Linux:
+                // FUNKTIONIERT Erben: java -jar "/home/stefan/Desktop/ndpi-to-ome-tiff-converter-v1.5/ndpi-converter.jar"  -i 2 -c lzw -s  "/home/stefan/Desktop/test Scan.ndpi"
+                // sudo java -jar /home/andreas/IdeaProjects/nucleij/lib/ndpi-converter/ndpi-converter.jar -i 2 -c lzw -s /home/andreas/Schreibtisch/Scans/N2700-14\ 5\ HE\ -\ 2016-06-06\ 14.57.00.ndpi
+            }
+            else if (OS.contains("windows")) {
+                System.out.println("Your OS is Windows");
+                p = Runtime.getRuntime().exec("java -jar \"" + absolutePathofNdpiJar + "\" -i 2 -c lzw -s \"" + filePath + "\" \"" + outputpath.getParent() + "\"");
+            }
+            else if (OS.contains("mac")) {
+                p = Runtime.getRuntime().exec("java -jar \"" + absolutePathofNdpiJar + "\" -i 2 -c lzw -s \"" + filePath + "\" \"" + outputpath.getParent() + "\"");
+                System.out.println("Your OS is Mac OS");
+            }
+            else {
+                System.out.println("Your OS is not supported!");
+                //TODO Möglicherweise ConvertProzess beenden und not supported Dialog anzeigen
+                p = null;
+            }
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         /*try {
@@ -237,10 +265,9 @@ public class AnalyzerConverterTask extends SwingWorker<String, String> {
             e.printStackTrace();
         }*/
 
-            String renameFileName = "_".concat(ndpiConverter.getMagnification().toLowerCase().concat(".ome.tif"));
-            String newTifListElement = filePath.replace(".ndpi", renameFileName);
-            System.out.println("Der Filename nach dem Konvertieren ist:" + newTifListElement);
-            glScanAnalyzer.addTifToList(newTifListElement);
-        }
-
+        String renameFileName = "_".concat(ndpiConverter.getMagnification().toLowerCase().concat(".ome.tif"));
+        String newTifListElement = filePath.replace(".ndpi", renameFileName);
+        System.out.println("Der Filename nach dem Konvertieren ist:" + newTifListElement);
+        glScanAnalyzer.addTifToList(newTifListElement);
+    }
 }
